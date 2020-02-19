95c10fc24405275027ddcf9919e819cfd0865981
danman15726 <danman15726@gmail.com>
2020-02-18 22:40:15 -0600

    initialized files. Added bootstrap in the index.html, and made a components folder full of javascript components. As of now the Bootstrap Card code works.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+import Card from "./components/card.js";
+
+const App = () => (
+  <div className = "container">
+    {/* <Jumbotron/> */}
+    <Card/>
+  </div>
+  
+
+
+);
+  
+export default App;
---
+++ src/components/card.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Card = () => (
+    <div className="card" style={{width: "18rem"}}>
+        <img className="card-img-top" src="..." alt="Card image cap"></img>
+        <div className="card-body">
+
+        </div>
+    </div>
+
+
+
+    
+
+
+);
+
+
+
+
+
+
+
+
+
+export default Card;
