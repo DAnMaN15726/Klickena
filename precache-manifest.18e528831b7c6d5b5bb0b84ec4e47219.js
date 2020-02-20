4a01ba442e45dd2a4e820d4e9aaea5eaefd4a3be
danman15726 <danman15726@gmail.com>
2020-02-20 02:29:07 -0600

    Updates

---
+++ precache-manifest.18e528831b7c6d5b5bb0b84ec4e47219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd873577e16da755e4ba54e0ddd2276b",
+    "url": "/Klickena/index.html"
+  },
+  {
+    "revision": "5f3682486870be49a7eb",
+    "url": "/Klickena/static/css/main.68f5a604.chunk.css"
+  },
+  {
+    "revision": "700a0bfe9f6025d67732",
+    "url": "/Klickena/static/js/2.8c023074.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Klickena/static/js/2.8c023074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3682486870be49a7eb",
+    "url": "/Klickena/static/js/main.c2d94ab2.chunk.js"
+  },
+  {
+    "revision": "b9d0439638b225c2a949",
+    "url": "/Klickena/static/js/runtime-main.94528138.js"
+  },
+  {
+    "revision": "1fc7d118b63605d52238d5d600f5bb03",
+    "url": "/Klickena/static/media/fr-title.1fc7d118.ttf"
+  }
+]);
